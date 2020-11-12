d1537dcf485f8a5a7f42c2d06b3c7c6ce3b23f56
kartikg2007 <73594173+kartikg2007@users.noreply.github.com>
2020-11-12 09:49:54 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+//Creating variables
+var car,wall;
+var speed, weight;
+var deformation;
+
+function setup() {
+  createCanvas(1600,400);
+
+  car = createSprite(50, 200, 100, 175);
+
+  wall = createSprite(1500,200,60,height/2);
+  wall.shapeColor= color(80,80,80);
+
+  speed=random(55,90);
+  weight = random(400,1500);
+}
+
+function draw() {
+  background(255,255,255); 
+  
+  isTouching();
+  deform();
+
+  car.velocityX = speed;
+
+  if(isTouching(car,wall)){
+    if(deformation<100){
+      car.shapeColor = color(0,255,0);
+    }
+
+    if(deformation>100&& deformation<180){
+      car.shapeColor = color(230,230,0);
+    }
+
+    if(deformation>180){
+      car.shapeColor = color(255,0,0);
+    }
+  }
+
+
+
+  drawSprites();
+}
+
+function isTouching(obj1,obj2){
+  if(obj1.x-obj2.x<obj1.width/2+obj2.width/2&&
+    obj2.x-obj1.x<obj1.width/2+obj2.width/2&&
+    obj1.y-obj2.y<obj1.height/2+obj2.height/2&&
+    obj2.y-obj1.y<obj1.height/2+obj2.height/2){
+
+      return true;
+    }
+    else{
+      return false;
+    }
+} 
+
+function deform(){
+  deformation = (0.5*weight*speed*speed)/22500
+}
